397b442ae3e0fe2e00d224457fd2ee751881bf94
daniel vicario <danigv.email@gmail.com>
2019-07-19 23:50:27 +0200

    fixed alt tag issue

--- src/Task.js
+++ src/Task.js
@@ -13,9 +13,9 @@ export default class Task extends Component {
                 {
                     this.props.done 
                     ?
-                    <img src={checkboxEnabled} className="done" onClick={() => this.props.toggleDone(this.props._id)}></img>
+                    <img src={checkboxEnabled} className="done" alt="" onClick={() => this.props.toggleDone(this.props._id)}></img>
                     :
-                    <img src={checkbox} className="done" onClick={() => this.props.toggleDone(this.props._id)}></img>
+                    <img src={checkbox} className="done" alt="" onClick={() => this.props.toggleDone(this.props._id)}></img>
                 }
                 
                 <svg className="favourite" style={{fill: this.props.favourited ? "yellow" : "grey"}} onClick={() => this.props.toggleFavourite(this.props._id)} xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z" /></svg>
